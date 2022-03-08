9c3c9bb720043c384db17ef8d04fd45ce8c9fb25
puimekster <hoanhao18102000@gmail.com>
2022-03-09 00:21:26 +0700

    update

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Cards from "../Cards";
+import Footer from "../Footer";
 import HeroSection from "../HeroSection";
 
 const Home = () => {
 	return (
 		<>
 			<HeroSection />
+			<Cards />
+			<Footer />
 		</>
 	);
 };
